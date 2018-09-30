e5e6cf17a045e3bbba7241c2827923d566a5b16a
Vlad Novosadenko <novosadenko.v23@gmail.com>
2018-09-30 06:01:07 +0300

    domain add

--- DjangoBegin/settings.py
+++ DjangoBegin/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['127.0.0.1', 'polls-world-cup.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'polls-world-cup.herokuapp.com', 'blog-start-django.com']
 
 
 # Application definition
