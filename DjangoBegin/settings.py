474b432f516726a3f35550f9d04ec5ddf4e4e2f6
Vlad Novosadenko <novosadenko.v23@gmail.com>
2018-09-30 05:50:01 +0300

    suk

--- DjangoBegin/settings.py
+++ DjangoBegin/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['127.0.0.1', 'django-polls-blog-train.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'polls-world-cup.herokuapp.com']
 
 
 # Application definition
