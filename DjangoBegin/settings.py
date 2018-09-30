76f8bd103785ea40ab56873560cde7d0dc9e20c8
Vlad Novosadenko <novosadenko.v23@gmail.com>
2018-09-30 05:09:53 +0300

    Update Django for whitenoise static

--- DjangoBegin/settings.py
+++ DjangoBegin/settings.py
@@ -133,13 +133,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    '/static/',
-]
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+#STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
